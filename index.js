af47411c394738cb885843e401ef74ec9359f0ec
bslee <niceinno@gmail.com>
2020-09-03 14:41:08 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://lbs:abcd1234@mall.l9hom.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
